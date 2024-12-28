b45211c1a287c4bbd468bfce515091e11fb72579
Murtaza Hussaini <159608043+murtazahussainii@users.noreply.github.com>
2024-12-28 14:16:39 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -12,18 +12,6 @@ console.log("hello world")
 console.log("hello world")
 console.log("hello world")
 
-console.log("hello world")console.log("hello world")
-
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")console.log("hello world")
 console.log("hello world")
 
-
-console.log("hello world")
-
-
-
-console.log("hello world")
-console.log("hello world")
 
